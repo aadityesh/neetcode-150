aee3d33ced1782e13469d4067d4ee3c557f8326d
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-01-29 01:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.56%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,14 +1,37 @@
+
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
+    int findMin(vector<int>& a) {
 
-        // brute - force 
-        int n = nums.size();
+        // brute - force
+        int n = a.size();
+        /*
         int mini = nums[0];
         for(int i = 1; i < n; i++) {
            mini = min(mini, nums[i]);
         }
         return mini;
-        
+        */
+
+        int low = 0;
+        int high = n - 1;
+        int mini = INT_MAX;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (a[low] <= a[mid]) {
+                mini = min(mini, a[low]);
+                low = mid + 1;
+
+            } else {
+
+                mini = min(mini, a[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return mini;
     }
 };
