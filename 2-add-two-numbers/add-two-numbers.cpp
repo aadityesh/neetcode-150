9ddcb902844df455fc849f20628a9728662ffd7a
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-06 12:15:20 +0530

    Time: 16 ms (85.58%) | Memory: 76.1 MB (12.89%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -12,6 +12,72 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
+        /* ListNode* res = new ListNode();
+         ListNode* tail = res;
+
+         ListNode* h1 = l1;
+         ListNode* h2 = l2;
+
+         int carry = 0;
+
+         while (h1 && h2) {
+
+             int sum = h1->val + h2->val + carry;
+
+             if ((sum / 10) > 0)
+                 carry = sum / 10;
+             else
+                 carry = 0;
+
+             ListNode* curr = new ListNode(sum % 10);
+             tail->next = curr;
+             tail = curr;
+
+             h1 = h1->next;
+             h2 = h2->next;
+         }
+
+         while (h1) {
+
+             int sum = h1->val + carry;
+             if ((sum / 10) > 0)
+                 carry = sum / 10;
+             else
+                 carry = 0;
+
+             ListNode* curr = new ListNode(sum % 10);
+             tail->next = curr;
+             tail = curr;
+
+             h1 = h1->next;
+         }
+
+         while (h2) {
+
+             int sum = h2->val + carry;
+
+             if ((sum / 10) > 0)
+                 carry = sum / 10;
+             else
+                 carry = 0;
+
+             ListNode* curr = new ListNode(sum % 10);
+             tail->next = curr;
+             tail = curr;
+
+             h2 = h2->next;
+         }
+
+         if (carry != 0) {
+             ListNode* curr = new ListNode(carry);
+             tail->next = curr;
+             tail = curr;
+         }
+
+         return res->next;*/
+
+        // Clean
+
         ListNode* res = new ListNode();
         ListNode* tail = res;
 
@@ -20,57 +86,26 @@ public:
 
         int carry = 0;
 
-        while (h1 && h2) {
+        while (h1 || h2) {
 
-            int sum = h1->val + h2->val + carry;
-            if ((sum / 10) > 0)
-                carry = sum / 10;
-            else
-                carry = 0;
+            int val1 = (h1 != NULL) ? h1->val : 0;
+            int val2 = (h2 != NULL) ? h2->val : 0;
 
-            ListNode* curr = new ListNode(sum % 10);
-            tail->next = curr;
-            tail = curr;
+            int sum = (val1 + val2 + carry);
+            carry = sum / 10;
 
-            h1 = h1->next;
-            h2 = h2->next;
-        }
+            ListNode* newNode = new ListNode(sum % 10);
+            tail->next = newNode;
+            tail = newNode;
 
-        while (h1) {
-
-            int sum = h1->val + carry;
-            if ((sum / 10) > 0)
-                carry = sum / 10;
-            else
-                carry = 0;
-
-            ListNode* curr = new ListNode(sum % 10);
-            tail->next = curr;
-            tail = curr;
-
-            h1 = h1->next;
-        }
-
-        while (h2) {
-
-            int sum = h2->val + carry;
-            
-            if ((sum / 10) > 0)
-                carry = sum / 10;
-            else
-                carry = 0;
-
-            ListNode* curr = new ListNode(sum % 10);
-            tail->next = curr;
-            tail = curr;
-
-            h2 = h2->next;
+            if (h1 != NULL)
+                h1 = h1->next;
+            if (h2 != NULL)
+                h2 = h2->next;
         }
 
         if (carry != 0) {
-            ListNode* curr = new ListNode(carry);
-            tail->next = curr;
-            tail = curr;
+            tail->next = new ListNode(carry);
         }
 
         return res->next;
