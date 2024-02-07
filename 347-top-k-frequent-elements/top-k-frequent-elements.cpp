fe735b8243341017e8ca974a1c0e7ed152b55363
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-07 11:17:37 +0530

    Time: 35 ms (5.31%) | Memory: 16.1 MB (29.34%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        map<int, int> mpp;
+        vector<int> temp;
+
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+
+        int cnt = 0;
+        int currElem = -1;
+
+        while(k--) {
+            
+            for(auto i : mpp) {
+
+                if(i.second > cnt) {
+                    cnt = i.second;
+                    currElem = i.first;
+
+                }
+                
+            }
+
+            if(find(temp.begin(), temp.end(), currElem) == temp.end())
+                temp.push_back(currElem);
+            
+            cnt = 0;
+            mpp[currElem] = -1;
+            currElem = -1;
+
+        }
+
+        return temp;     
+
+
+    }
+};
