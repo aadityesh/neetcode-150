ff11e962fa32c05ab432cea88f51a35770f58daa
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-13 10:27:55 +0530

    Time: 26 ms (26.74%) | Memory: 13.8 MB (20.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,6 +4,23 @@ public:
 
         int n = s.length();
         int res = 0;
+
+        // Brute
+    /*
+        for (int i = 0; i < n; i++) {
+            unordered_set<char> uset;
+            for (int j = i; j < n; j++) {
+                uset.insert(s[j]);
+                // check unique
+                if(uset.size() == j - i + 1) {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+    */
+
+        // Optimal
+        
         int left = 0;
         int right = 0;
         // [ l ... r ] -> hold substr w/o repeating characs
@@ -17,8 +34,7 @@ public:
             res = max(res, right - left + 1);
             uset.insert(s[right++]);
         }
-
+        
         return res;
     }
-}
-;
+};
