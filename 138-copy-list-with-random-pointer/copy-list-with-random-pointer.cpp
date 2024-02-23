c763c8e4c80b83be9f6c5a3fef69f4ec4d9c3483
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-24 03:19:23 +0530

    Time: 3 ms (93.20%) | Memory: 12.7 MB (56.43%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -18,6 +18,8 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
 
+        // Brute
+        /*
         unordered_map<Node*, Node*> umap;
         Node* curr = head;
 
@@ -33,7 +35,51 @@ public:
             curr = curr->next;
         }
         return umap[head];
+        */
 
+        // Optimal
+        /*
+        1. Insert Copy nodes in between
+        2. Connect random ptr
+        3. Connect next ptr using dummy node
+        */
+        Node* curr = head;
+        while (curr) {
+            Node* newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = curr->next->next;
+        }
+        curr = head;
+        while (curr) {
+            Node* newNode = curr->next;
+            if (curr->random)
+                newNode->random = curr->random->next;
+            curr = curr->next->next;
+        }
 
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+        curr = head;
+
+        while (curr) {
+            tail->next = curr->next;
+            tail = tail->next;
+
+            curr->next = curr->next->next;
+            curr = curr->next;
+        }
+
+        return dummy->next;
+
+        // Printing
+
+        // curr = head;
+        // while(curr) {
+        //     cout << curr->val << " ";
+        //     curr = curr->next;
+        // }
+
+        // return head;
     }
 };
