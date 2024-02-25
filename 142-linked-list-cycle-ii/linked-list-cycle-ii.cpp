ea7492662a9265d6562af99c53eafc6503305193
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-25 15:29:55 +0530

    Time: 9 ms (51.58%) | Memory: 10.1 MB (53.61%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -11,6 +11,8 @@ public:
     ListNode* detectCycle(ListNode* head) {
 
         // Set - Node*
+        // Brute - Keeping Track of Visited Nodes.
+        /*
         unordered_set<ListNode*> stt;
         ListNode* curr = head;
         while (curr) {
@@ -19,6 +21,33 @@ public:
 
             stt.insert(curr);
             curr = curr->next;
+        }
+
+        return NULL;
+        */
+
+        // 2 * (dis of slow) = dis of fast
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool res = false;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                res = true;
+                break;
+            }
+        }
+
+        if (res) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+
+            return slow;
         }
 
         return NULL;
