a1a0c2b191ddff4d3565f144e4e9e6de1300850b
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-02-29 03:52:24 +0530

    Time: 87 ms (81.33%) | Memory: 60.7 MB (89.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
-        
-        // Brute - two nested loops
-        // Better - HashMap
 
-        // Best 
+        // Brute - two nested loops
+        /*int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;*/
+
+        // Better - HashMap
+        // unordered_map<int, int> umap;
+        // for (auto i : nums) {
+        //     umap[i]++;
+        // }
+        // for (auto i : umap) {
+        //     if (i.second > 1)
+        //         return false;
+        // }
+        // return true;
+
+        // Best
         sort(nums.begin(), nums.end());
 
-        for(int i = 1; i < nums.size(); i++)
-        {
-            if(nums[i - 1] == nums[i])
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] == nums[i])
                 return true;
         }
 
         return false;
-
     }
 };
