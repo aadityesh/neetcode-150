b7f8486e223cc1fbdf645b8b0b04b19aad6206f9
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-03-06 16:35:34 +0530

    Time: 11 ms (71.11%) | Memory: 11.1 MB (63.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,38 +3,59 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n = s.length();
-        int res = 0;
+        // int res = 0;
 
         // Brute
-    /*
-        for (int i = 0; i < n; i++) {
-            unordered_set<char> uset;
-            for (int j = i; j < n; j++) {
-                uset.insert(s[j]);
-                // check unique
-                if(uset.size() == j - i + 1) {
-                    res = max(res, j - i + 1);
+        /*
+            for (int i = 0; i < n; i++) {
+                unordered_set<char> uset;
+                for (int j = i; j < n; j++) {
+                    uset.insert(s[j]);
+                    // check unique
+                    if(uset.size() == j - i + 1) {
+                        res = max(res, j - i + 1);
+                    }
                 }
             }
-        }
-    */
+        */
 
         // Optimal
-        
-        int left = 0;
-        int right = 0;
-        // [ l ... r ] -> hold substr w/o repeating characs
-        unordered_set<char> uset;
-        while (right < n) {
-            while (uset.find(s[right]) != uset.end()) {
-                uset.erase(s[left]);
-                left++;
+
+        // int left = 0;
+        // int right = 0;
+        // // [ l ... r ] -> hold substr w/o repeating characs
+        // unordered_set<char> uset;
+        // while (right < n) {
+        //     while (uset.find(s[right]) != uset.end()) {
+        //         uset.erase(s[left]);
+        //         left++;
+        //     }
+
+        //     res = max(res, right - left + 1);
+        //     uset.insert(s[right++]);
+        // }
+
+        // return res;
+
+        int i = 0;
+        int j = 0;
+
+        unordered_map<char, int> umap;
+        int res = 0;
+
+        while (j < n) {
+
+            umap[s[j]]++;
+
+            while (umap[s[j]] > 1) {
+                umap[s[i]]--;
+                i++;
             }
 
-            res = max(res, right - left + 1);
-            uset.insert(s[right++]);
+            res = max(res, j - i + 1);
+            j++;
         }
-        
+
         return res;
     }
 };
