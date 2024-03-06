54bd2e879bc392e23d86c96824de7c1b697795a2
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-03-06 15:36:42 +0530

    Time: 51 ms (22.21%) | Memory: 58.6 MB (60.69%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -5,26 +5,44 @@ public:
         int n = a.size();
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
+        // for (int i = 0; i < n; i++) {
 
-            int cntZero = 0;
-            int j = i;
+        //     int cntZero = 0;
 
-            for (int j = i; j < n; j++) {
+        //     for (int j = i; j < n; j++) {
 
-                if (a[j] == 0)
-                    cntZero++;
+        //         if (a[j] == 0)
+        //             cntZero++;
 
-                if (cntZero > 1) {
-                    ans = max(ans, j - i - 1);
-                    break;
-                }
-                if (cntZero <= 1) {
-                    ans = max(ans, j - i);
-                }
+        //         if (cntZero > 1) { // More than one Zero
+        //             ans = max(ans, j - i - 1);
+        //             break;
+        //         }
+
+        //         if (cntZero <= 1) { // No zero / One Zero
+        //             ans = max(ans, j - i);
+        //         }
+        //     }
+        // }
+
+        int i = 0;
+        int j = 0;
+        int cntZero = 0;
+
+        while (j < n) {
+
+            if (a[j] == 0)
+                cntZero++;
+
+            while (cntZero > 1) {
+                if (a[i] == 0)
+                    cntZero--;
+                i++;
             }
+            ans = max(ans, j - i + 1);
+            j++;
         }
 
-        return ans;
+        return ans - 1;
     }
 };
