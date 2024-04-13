5de75241adda6f6e16302d046079505fa60406b5
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-04-13 21:07:00 +0530

    Time: 8 ms (61.57%) | Memory: 14.3 MB (8.14%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -12,32 +12,67 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
-        vector<int> even;
-        vector<int> odd;
+        // Brute
+        /*
+         vector<int> even;
+         vector<int> odd;
 
+         ListNode* curr = head;
+         int idx = 1;
+         while (curr) {
+             if (idx % 2 == 0) {
+                 even.push_back(curr->val);
+                 idx++;
+             } else {
+                 odd.push_back(curr->val);
+                 idx++;
+             }
+             curr = curr->next;
+         }
+
+         curr = head;
+         for (int i = 0; i < odd.size(); i++) {
+             curr->val = odd[i];
+             curr = curr->next;
+         }
+         for (int i = 0; i < even.size(); i++) {
+             curr->val = even[i];
+             curr = curr->next;
+         }
+
+         return head;
+         */
+
+        // Better
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        vector<int> data;
+        // adding odd elems first
         ListNode* curr = head;
-        int idx = 1;
-        while (curr) {
-            if (idx % 2 == 0) {
-                even.push_back(curr->val);
-                idx++;
-            } else {
-                odd.push_back(curr->val);
-                idx++;
-            }
-            curr = curr->next;
+        while (curr && curr->next) {
+            data.push_back(curr->val);
+            curr = curr->next->next;
+        }
+
+        if (curr) {
+            data.push_back(curr->val);
+        }
+
+        // adding even elems second
+        curr = head->next;
+        while (curr && curr->next) {
+            data.push_back(curr->val);
+            curr = curr->next->next;
         }
 
         curr = head;
-        for (int i = 0; i < odd.size(); i++) {
-            curr->val = odd[i];
+        int i = 0;
+        for (int i = 0; i < data.size(); i++) {
+            curr->val = data[i];
             curr = curr->next;
         }
-        for (int i = 0; i < even.size(); i++) {
-            curr->val = even[i];
-            curr = curr->next;
-        }
-        
+
         return head;
     }
 };
