2b00b13670975f9b47a19c6524b1e449da967c1a
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-04-20 17:12:28 +0530

    Time: 3 ms (37.74%) | Memory: 8.4 MB (68.30%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,38 +1,75 @@
 class MyQueue {
-    stack<int> st1;
-    stack<int> st2;
-
 public:
+    stack<int> input;
+    stack<int> output;
+    /*
+    // Brute
+    class MyQueue {
+        stack<int> st1;
+        stack<int> st2;
+
+    public:
+        MyQueue() {}
+
+        void push(int x) {
+            while (!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+            st2.push(x);
+            while (!st2.empty()) {
+                st1.push(st2.top());
+                st2.pop();
+            }
+        }
+
+        int pop() {
+            int elem = st1.top();
+            st1.pop();
+            return elem;
+        }
+
+        int peek() { return st1.top(); }
+
+        bool empty() { return st1.size() == 0; }
+    };
+
+    */
     MyQueue() {}
 
-    void push(int x) {
-        while (!st1.empty()) {
-            st2.push(st1.top());
-            st1.pop();
-        }
-        st2.push(x);
-        while (!st2.empty()) {
-            st1.push(st2.top());
-            st2.pop();
-        }
-    }
+    void push(int x) { input.push(x); }
 
     int pop() {
-        int elem = st1.top();
-        st1.pop();
-        return elem;
+        if (!output.empty()) {
+            int elem = output.top();
+            output.pop();
+            return elem;
+        } else {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+            int elem = output.top();
+            output.pop();
+            return elem;
+        }
     }
 
-    int peek() { return st1.top(); }
+    int peek() {
+        if (!output.empty()) {
+            int elem = output.top();
+            // output.pop();
+            return elem;
+        } else {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+            int elem = output.top();
+            // output.pop();
+            return elem;
+        }
+    }
 
-    bool empty() { return st1.size() == 0; }
+    bool empty(){return (input.empty() && output.empty());};
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
