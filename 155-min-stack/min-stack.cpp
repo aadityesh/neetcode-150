b21639dc1044155df44e913e158a6d2c49707851
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-04-20 21:16:37 +0530

    Time: 20 ms (35.70%) | Memory: 20.9 MB (58.04%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,61 +1,37 @@
-class MinStack
-{
-    private:
-        stack<int> st1;
-        stack<int> st2;
-    public:
-        MinStack() {}
+class MinStack {
+    stack<int> st1;
+    stack<int> st2;
 
-    void push(int val)
-    {
-        st1.push(val);
-        if(st2.empty() || val <= st2.top()) st2.push(val);
+public:
+    MinStack() {}
+
+    void push(int x) {
+        st1.push(x);
+        if (st2.empty() || st2.top() >= x)
+            st2.push(x);
     }
 
-    void pop()
-    {
-        if(st1.top() == st2.top()) st2.pop();
+    void pop() {
+        if (st2.top() == st1.top()) {
+            st2.pop();
+        }
         st1.pop();
     }
 
-    int top()
-    {
-        return st1.top();
-    }
+    int top() { return st1.top(); }
 
-    int getMin()
-    {
-
-        // Brute Approach
-        // ALT: HashMap
-        
-        /*stack<int> cpy = st;
-        vector<int> a;
-
-        while (!cpy.empty())
-        {
-            a.push_back(cpy.top());
-            cpy.pop();
-
-        }
-
-        int mini = INT_MAX;
-        for (auto i: a)
-        {
-            if (i < mini)
-                mini = i;
-        }
-        return mini;*/
-
-        return st2.top();
+    int getMin() {
+        if (!st2.empty())
+           { return st2.top();}
+        return -1;
     }
 };
 
 /**
- *Your MinStack object will be instantiated and called as such:
- *MinStack* obj = new MinStack();
- *obj->push(val);
- *obj->pop();
- *int param_3 = obj->top();
- *int param_4 = obj->getMin();
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
  */
