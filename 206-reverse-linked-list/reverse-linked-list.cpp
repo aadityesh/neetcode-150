23e6991a529337df1cdcc379317bfe9b99279e84
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-05-07 11:59:20 +0530

    Time: 5 ms (28.96%) | Memory: 11.5 MB (91.54%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,71 +10,17 @@
  */
 class Solution {
 public:
-    // void insertAtHead(ListNode*& head, int val) {
-
-    //     ListNode* temp = new ListNode();
-    //     temp->val = val;
-    //     temp->next = head;
-    //     head = temp;
-    // }
-
-    ListNode* reverseNode(ListNode*& head) {
-
-         if(head == NULL || head->next == NULL)
-            return head;
-        
-        ListNode* newHead = reverseNode(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-
-        return newHead;
-
-    }
-
     ListNode* reverseList(ListNode* head) {
-
-        /*vector<int> a;
-        ListNode* temp = head;
-
-        while (temp != NULL) {
-            a.push_back(temp->val);
-            temp = temp->next;
-        }
-
-        for(auto i : a) cout << i << endl;
-
-
-        int n = a.size();
-
+        ListNode* prev = nullptr;
         ListNode* curr = head;
-        // curr->val = a[n - 1];
 
-        for (int i = n - 1; i >= 0; i--) {
-            head->val = a[i];
-            head = head->next;
-        }*/
-        /*
-
-        // Optimal - Iterative
-        ListNode * next, * prev = NULL;
-        ListNode *curr = head;
-
-        while(curr) {
-            next = curr->next;
+        while (curr) {
+            ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
 
         return prev;
-
-        */
-
-        // Optimal - Recursive
-      
-
-       return reverseNode(head);
-
     }
 };
