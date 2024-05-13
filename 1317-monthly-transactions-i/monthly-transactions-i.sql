5c4461e905ec8fbae729d6e9a6bd8cb83c22b26f
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-05-13 14:08:51 +0530

    Time: 1177 ms (44.23%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,9 +1,9 @@
 SELECT 
-    DATE_FORMAT(trans_date, "%Y-%m") AS month,
+    SUBSTR(trans_date,1,7) AS month,
     country,
     count(id) as trans_count,
     sum(state="approved") as approved_count,
     sum(amount) as trans_total_amount,
     sum(if(state='approved', amount, 0)) as approved_total_amount 
-FROM Transactions a
+FROM Transactions 
 GROUP BY month, country
