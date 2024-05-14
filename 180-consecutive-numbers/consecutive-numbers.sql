5e6716b09803305d79debabe5533c417c4f423f7
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-05-14 13:40:14 +0530

    Time: 898 ms (85.63%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -6,6 +6,11 @@
 -- GROUP BY a.num
 -- HAVING COUNT(a.num) >= 2
 
-SELECT distinct num as ConsecutiveNums
-from logs
-where (id+1, num) in (select * from logs) and (id+2, num) in (select * from logs)
+-- SELECT distinct num as ConsecutiveNums
+-- from logs
+-- where (id+1, num) in (select * from logs) and (id+2, num) in (select * from logs)
+
+SELECT distinct a.num as ConsecutiveNums
+from Logs a, Logs b, Logs c
+where (b.id - a.id = 1 AND c.id - b.id = 1)
+AND a.num = b.num AND b.num = c.num
