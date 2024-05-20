34bc2939cd3041594222bea85652928e46913f28
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-05-21 00:34:16 +0530

    Time: 39 ms (61.02%) | Memory: 16.9 MB (92.72%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,23 +8,58 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
 
-            ListNode* curr1 = headA;
-            ListNode* curr2 = headB;
+        ListNode* curr1 = headA;
+        ListNode* curr2 = headB;
 
-            set<ListNode*> ss;
-            while(curr1) {
-                ss.insert(curr1);
-                curr1 = curr1->next;
-            }
+        /*
+        set<ListNode*> ss;
+        while(curr1) {
+            ss.insert(curr1);
+            curr1 = curr1->next;
+        }
 
-            while(curr2) {
-                if(ss.find(curr2) != ss.end()) break;
-                curr2 = curr2->next;
-            }
+        while(curr2) {
+            if(ss.find(curr2) != ss.end()) break;
+            curr2 = curr2->next;
+        }
 
-            return curr2;
+        return curr2;
+        */
 
+        int n1 = 0;
+        int n2 = 0;
+
+        while (curr1) {
+            n1++;
+            curr1 = curr1->next;
+        }
+
+        while (curr2) {
+            n2++;
+            curr2 = curr2->next;
+        }
+
+        curr1 = headA;
+        curr2 = headB;
+        int diff = 0;
+
+        if (n1 > n2) {
+            diff = n1 - n2;
+            while (diff-- && curr1) curr1 = curr1->next;
+        }
+        else {
+            diff = n2 - n1;
+            while (diff-- && curr2) curr2 = curr2->next;
+        }
+
+        while(curr1 && curr2) {
+            if(curr1 == curr2) break;
+            curr1 = curr1->next;
+            curr2 = curr2->next;
+        }
+
+        return curr1;
     }
 };
