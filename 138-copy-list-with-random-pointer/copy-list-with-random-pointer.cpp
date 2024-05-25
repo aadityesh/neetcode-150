84f62d708c35943d5a57a6fc758bfc669af5e021
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-05-25 19:14:58 +0530

    Time: 7 ms (47.95%) | Memory: 12.7 MB (89.24%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,25 +1,68 @@
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
-        unordered_map<Node*, Node*> umap;
+
+        /* unordered_map<Node*, Node*> umap;
+         Node* curr = head;
+
+         while(curr) {
+             umap[curr] = new Node(curr->val);
+             curr = curr->next;
+         }
+         curr = head;
+         while(curr) {
+
+             Node* cpyNode = umap[curr];
+             cpyNode->next = umap[curr->next];
+             cpyNode->random = umap[curr->random];
+
+             curr = curr->next;
+         }
+
+         return umap[head];
+     */
+
         Node* curr = head;
 
-        while(curr) {
-            umap[curr] = new Node(curr->val);
-            curr = curr->next;
+        // Insert in between
+        while (curr) {
+
+            Node *newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+
+            curr = curr->next->next;
+
         }
+        // Connect Random ptr
         curr = head;
-        while(curr) {
+        while (curr) {
 
-            Node* cpyNode = umap[curr];
-            cpyNode->next = umap[curr->next];
-            cpyNode->random = umap[curr->random];
+            Node* cpy = curr->next;
+            if (curr->random)
+                cpy->random = curr->random->next;
+            else
+                cpy->random = NULL;
 
-            curr = curr->next;
+            curr = curr->next->next;
+
         }
 
-        return umap[head];
+        // Connect next ptr
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+        curr = head;
 
+        while (curr) {
+           
+            tail->next = curr->next;
+            tail = tail->next;
+
+            curr->next = curr->next->next;
+            curr = curr->next;          
+            
+        }
+
+        return dummy->next;
     }
 };
