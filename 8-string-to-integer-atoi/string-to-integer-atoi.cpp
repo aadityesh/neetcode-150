2d4c1ace17c5ecbb9d9e0069a31924919f3df3d3
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-06-06 12:11:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,30 +1,37 @@
 class Solution {
-   private:
-   int atoi(string s, int i, int ans, int sign) {
-       if (i == s.length() || s[i] < '0' || s[i] > '9') {
-           return ans * sign;
-       }
-
-       int no = s[i] - '0';
-       if ((ans > (INT_MAX / 10)) || (ans * 10 > INT_MAX - no)) {
-           return sign == 1 ? INT_MAX : INT_MIN;
-       }
-       ans = ans * 10 + no;
-       return atoi(s, i + 1, ans, sign);
-   }
 public:
-   int myAtoi(string s) {
-       int i = 0;
-       int ans = 0;
-       int sign = 1;  
-       for( ; i < s.length() && s[i] == ' '; ) 
-           i++; 
-       
-       if(s[i] == '-' || s[i] == '+') {
-           if(s[i] == '-' )
-               sign = -1;
-           i++;
-       }
-       return atoi(s, i, ans, sign);
-   }
+    int recursiveAtoi(string s, int i, int neg, int res) {
+
+        if (i >= s.length() || s[i] < '0' || s[i] > '9')
+            return neg * res;
+
+        int integer = s[i] - '0';
+        if ((res > (INT_MAX / 10)) || (res * 10 > (INT_MAX - integer)))
+            return neg > 0 ? INT_MAX : INT_MIN;
+
+        res = res * 10 + integer;
+
+        return recursiveAtoi(s, i + 1, neg, res);
+    }
+    int myAtoi(string s) {
+
+        int n = s.length();
+        int neg = 1;
+
+        int i = 0;
+        while (s[i] == ' ')
+            i++;
+        if (s[i] == '-') {
+            neg = -1;
+            i++;
+        } else if (s[i] == '+')
+            i++;
+
+        int result = recursiveAtoi(s, i, neg, 0);
+        // result = neg ? -result : result;
+
+        cout << INT_MAX << endl;
+
+        return result;
+    }
 };
