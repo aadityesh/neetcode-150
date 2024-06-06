d76aadee3f0a9449a91e90cf93e4572e0381872b
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-06-06 19:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (21.31%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 1.0;
-        long pow = abs(n);
+        
+        if(n == 0) return 1;
 
-        while(pow) {
-
-            if(pow & 1) {
-                res = res * x;
-                pow = pow - 1;
-            }
-
-            else {
-                x = x * x;
-                pow /= 2;
-            }
-
+        if(n < 0) {
+            x = 1 / x;
+            n = abs(n);
         }
 
-        if(n < 0) res = 1.0 / res;
+        if(n & 1) { // if n is odd 
+            return x * myPow(x*x, n / 2);
+        }
 
-        return res;
-
+        else
+            return myPow(x*x, n / 2);
     }
 };
