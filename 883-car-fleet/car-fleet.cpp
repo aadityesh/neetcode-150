7e386486938d9079e3ba9939eda627ae675fea6b
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-06-29 14:46:17 +0530

    Time: 127 ms (71.34%) | Memory: 82 MB (73.89%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& pos, vector<int>& speed) {
+
+        int n = pos.size();
+        stack<double> st;
+        vector<pair<int, int>> input;
+
+        for (int i = 0; i < n; i++) {
+            input.push_back({pos[i], speed[i]});
+        }
+
+        sort(input.begin(), input.end());
+
+        for(int i = n - 1; i >= 0 ; i--) {
+
+            double time = double(target - input[i].first) / double(input[i].second);
+            // if(time <= st.top()) -> required condition
+            if(st.empty() || time > st.top()) {
+                st.push(time);
+            }
+
+        }
+
+        return int(st.size());
+
+    }
+};
