dae511c06deef30473de3e1f80c98c365ca9b57b
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-07-06 22:35:08 +0530

    Time: 46 ms (82.46%) | Memory: 48.5 MB (78.18%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -1,27 +1,21 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var count = 0;
+// var count = 0;
+// var createCounter = function (n) {
+//     let flag = 1;
+//     return function () {
+//         if(flag) {
+//             flag = 0;
+//             count = n;
+//             return n;
+//         } else {
+//             count = count + 1; 
+//             return count;
+//         }
+
+//     };
+// };
 
 var createCounter = function (n) {
-    let flag = 1;
     return function () {
-        if(flag) {
-            flag = 0;
-            count = n;
-            return n;
-        } else {
-            count = count + 1; 
-            return count;
-        }
-
+        return n++;
     };
 };
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
