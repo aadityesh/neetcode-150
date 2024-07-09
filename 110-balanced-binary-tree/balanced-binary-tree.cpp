226d0a5376250a4837225da06f18a127df14a300
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-07-09 16:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (75.65%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,6 +12,8 @@
  */
 class Solution {
 public:
+    // Brute
+    /*
     int height(TreeNode* root) {
         if (root == NULL)
             return 0;
@@ -38,6 +40,26 @@ public:
         if (!left || !right)
             return false;
 
+        return true;
+    }
+    */
+
+    int height(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        if (abs(lh - rh) > 1 || lh == -1 || rh == -1)
+            return -1;
+
+        return 1 + max(lh, rh);
+    }
+    bool isBalanced(TreeNode* root) {
+
+        if (height(root) == -1)
+            return false;
         return true;
     }
 };
