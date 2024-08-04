137281fdabeb4dfd41a9981bd7a1eb29cf922396
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-08-04 11:51:29 +0530

    Time: 14 ms (50.07%) | Memory: 27.9 MB (14.84%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,7 +1,8 @@
 
 class Solution {
 public:
-
+    // Brute
+    /*
     int findPosition(vector<int>& inorder, int elem) {
         for(int i = 0; i < inorder.size(); i++) {
             if(inorder[i] == elem) return i;
@@ -10,32 +11,55 @@ public:
         return -1;
 
     }
+    */
 
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int &index,
-    int istart, int iend) {
+    int findPosition(unordered_map<int, int>& umap, int elem) {
+        if (umap.find(elem) == umap.end())
+            return -1;
 
-        if(index >= preorder.size() || istart > iend ) return NULL;
+        return umap[elem];
+    }
 
+    // TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& index,
+    //                 int istart, int iend) {
+
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& index,
+                    int istart, int iend, unordered_map<int, int>& umap) {
+
+        if (index >= preorder.size() || istart > iend)
+            return NULL;
+
+        // EXTRACTION OF ROOT
         int currentRootData = preorder[index++]; // updation of index
         TreeNode* currentRoot = new TreeNode(currentRootData);
-        int currentRootPosition = findPosition(inorder, currentRootData); // helps us in finding L,R
 
-        currentRoot->left = solve(preorder, inorder, index, istart, currentRootPosition-1);
-        currentRoot->right = solve(preorder, inorder, index, currentRootPosition+1, iend);
-        
+        // EXTRACTION OF L, R
+        int currentRootPosition =
+            findPosition(umap, currentRootData); // helps us in finding L,R
+        currentRoot->left =
+            solve(preorder, inorder, index, istart, currentRootPosition - 1, umap);
+        currentRoot->right =
+            solve(preorder, inorder, index, currentRootPosition + 1, iend, umap);
 
-
-    return currentRoot;
-
+        return currentRoot;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
         int n = preorder.size();
         int extractRootIndex = 0;
-        TreeNode* root = solve(preorder, inorder, extractRootIndex, 0, n-1); // inorder-start, inorder-end
+
+        unordered_map<int, int> umap;
+        for (int i = 0; i < n; i++) {
+            umap[inorder[i]] = i;
+        }
+
+        // TreeNode* root = solve(preorder, inorder, extractRootIndex, 0,
+        //                        n - 1); // inorder-start, inorder-end
+
+        TreeNode* root = solve(preorder, inorder, extractRootIndex, 0,
+                               n - 1, umap); // inorder-start, inorder-end
 
         return root;
-
-
     }
 };
