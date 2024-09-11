26949c67cde6ae126b190314baca62bea0327ec7
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-09-11 10:06:22 +0530

    Time: 170 ms (45.44%) | Memory: 78.9 MB (34.63%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -5,24 +5,18 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
         vector<vector<int>> res;
-        priority_queue<int, vector<int>, greater<int>> pq;
-        unordered_map<int, vector<vector<int>>> umap;
-
-        for (vector<int>& i : points) {
-            int x = i[0];
-            int y = i[1];
-            int distance = square(x) + square(y);
-            umap[distance].push_back({x, y});
-            pq.push(distance);
+        priority_queue<vector<int>> pq;
+        
+        for(vector<int> &v : points) {
+            int dist = square(v[0]) + square(v[1]);
+            pq.push({dist, v[0], v[1]});
+            // maintain size K
+            if(pq.size() > k) pq.pop();
         }
 
-        while (res.size() != k) {
-            int distance = pq.top();
-            for (vector<int>& point : umap[distance]) {
-                res.push_back(point); 
-                if (res.size() == k)
-                    break; 
-            }
+        while(!pq.empty()) {
+            vector<int> point = pq.top();
+            res.push_back({point[1], point[2]});
             pq.pop();
         }
 
