d2382dc3fd7fecb90e9042c43eba919fe895a58c
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-09-25 11:52:22 +0530

    Time: 7 ms (9.55%) | Memory: 10.3 MB (32.55%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,67 +1,32 @@
 class Solution {
 public:
-    int factorial(int n) {
-        int result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
+    void dfs(vector<int>& input, vector<vector<int>>& result,
+             vector<int>& current, unordered_set<int>& hashSet) {
+
+        if (current.size() == input.size()) {
+            result.push_back(current);
+            return;
         }
+
+        for (int i = 0; i < input.size(); i++) {
+            if (hashSet.find(input[i]) == hashSet.end()) {
+                current.push_back(input[i]);
+                hashSet.insert(input[i]);
+                dfs(input, result, current, hashSet);
+                current.pop_back();
+                hashSet.erase(input[i]);
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& input) {
+
+        vector<vector<int>> result;
+        vector<int> current;
+        unordered_set<int> hashSet;
+
+        dfs(input, result, current, hashSet);
+
         return result;
-    }
-
-    vector<int> nextPermutation(vector<int> input) {
-
-        /*
-            1. Find break point
-            2. swap with the next greater value
-            3. call reverse for range (breakpoint+1, n)
-
-            // 2 1 5 4 3
-            // 2 3 5 4 1
-            // 2 3 1 4 5
-
-        */
-        int n = input.size();
-        int breakPointIndex = -1;
-        for (int i = n-2; i >= 0; i--) {
-            if (input[i] < input[i+1]) {
-                breakPointIndex = i;
-                break;
-            }
-        }
-
-        if (breakPointIndex == -1) {
-            reverse(input.begin(), input.end());
-            return input;
-        }
-
-        for (int i = n - 1; i > breakPointIndex; i--) {
-            if (input[i] > input[breakPointIndex]) {
-                swap(input[breakPointIndex], input[i]);
-                reverse(input.begin() + breakPointIndex + 1, input.end());
-                break;
-            }
-        }
-
-        return input;
-    }
-
-    vector<vector<int>> permute(vector<int>& nums) {
-
-        int n = nums.size();
-        if (n == 1)
-            return {nums};
-
-        int range = factorial(n);
-        vector<vector<int>> res;
-        vector<int> current = nums;
-
-        // for(int i : current) cout << i << " ";
-        for (int i = 0; i < range; i++) {
-            vector<int> temp = nextPermutation(current);
-            res.push_back(temp);
-            current = temp;
-        }
-
-        return res;
     }
 };
