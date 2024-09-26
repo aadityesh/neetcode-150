6a5eb7c9a66210a392f95d5c8274a34948e47f2d
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-09-26 14:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.51%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<int>& input, vector<vector<int>>& result,
-             vector<int>& current, vector<bool>& hashSet) {
-
-        if (current.size() == input.size()) {
-            result.push_back(current);
-            return;
+    vector<vector<int>> res;
+    void dfs(vector<int>& input, int start) {
+        if (start == input.size()) {
+            res.push_back(input);
         }
-
-        for (int i = 0; i < input.size(); i++) {
-            if (hashSet[i] == false) {
-                current.push_back(input[i]);
-                hashSet[i] = true;
-                dfs(input, result, current, hashSet);
-                current.pop_back();
-                hashSet[i] = false;
-            }
+        for (int i = start; i < input.size(); i++) {
+            swap(input[start], input[i]);
+            dfs(input, start + 1);
+            swap(input[start], input[i]);
         }
     }
-
-    vector<vector<int>> permute(vector<int>& input) {
-
-        vector<vector<int>> result;
-        vector<int> current;
-        vector<bool> hashSet(input.size(), false);
-
-        dfs(input, result, current, hashSet);
-
-        return result;
+    vector<vector<int>> permute(vector<int>& nums) {
+        dfs(nums, 0);
+        return res;
     }
 };
