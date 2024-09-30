35abbc85925388b963b8dff28b4dddd1b4151b90
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-09-30 21:33:01 +0530

    Time: 3 ms (52.92%) | Memory: 10 MB (43.38%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,18 +1,46 @@
+
 class Solution {
 public:
-    vector<vector<int>> res;
-    void dfs(vector<int>& input, int start) {
-        if (start == input.size()) {
-            res.push_back(input);
+    void dfs(vector<vector<int>>& res, vector<bool>& hash, vector<int>& nums,
+             vector<int>& temp) {
+
+        if (temp.size() == nums.size()) {
+            res.push_back(temp);
+            return;
         }
-        for (int i = start; i < input.size(); i++) {
-            swap(input[start], input[i]);
-            dfs(input, start + 1);
-            swap(input[start], input[i]);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash[i] == false) {
+                hash[i] = true;
+                temp.push_back(nums[i]);
+                dfs(res, hash, nums, temp);
+                hash[i] = false;
+                temp.pop_back();
+            }
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        dfs(nums, 0);
+
+        int n = nums.size();
+
+        vector<vector<int>> res;
+        vector<int> temp;
+        vector<bool> hash(n, false);
+
+        dfs(res, hash, nums, temp);
+
         return res;
     }
+    /*
+
+        _ _ _
+        3 2 1 = N!
+
+                1 2 3
+            1 2 3
+        1 2 3
+
+
+    */
 };
