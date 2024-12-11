37b581f3ea47c751e7dc2433aac9a6910e2d60f4
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-11 19:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (16.03%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        int j = n - 1;
 
-        if (n == 1)
-            return;
+        int n = nums.size();
+        int current = 0; // index to fill from the start
 
         for (int i = 0; i < n; i++) {
-
-            if (nums[i] == 0) {
-                j = i;
-                while (j <= n - 2 && nums[j] == 0)
-                    j++;
-                // cout << i << " " << j << endl;
-
-                swap(nums[i], nums[j]);
+            if (nums[i] != 0) {
+                nums[current++] = nums[i];
             }
         }
+
+        while (current < n) {
+            nums[current++] = 0;
+        }
     }
-};
+}
+
+;
