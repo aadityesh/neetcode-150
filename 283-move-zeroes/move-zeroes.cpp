78db9ad70b41d46a9305b6602e15fac243271b7c
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-11 22:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (10.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -15,11 +15,21 @@ public:
             nums[current++] = 0;
         }*/
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                swap(nums[current], nums[i]);
-                current
-                ++;
+        /*
+        - R is non-zero
+            - make the swap move to the next
+        - R is zero
+            - L will stand on the first pos from left (start, index = 0) to replaced with
+              a non-zero element
+
+        // 1 2 3 1 2 0 8 7 9 0
+        */
+
+        int L = 0; // contains position to fill with non-zero element
+        for (int R = 0; R < n; R++) {
+            if (nums[R] != 0) {
+                swap(nums[L], nums[R]);
+                L++;
             }
         }
     }
