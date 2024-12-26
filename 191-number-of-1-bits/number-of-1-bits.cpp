580ea9f3e760253bb4ab17cbf64b2a2ec7656f30
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-26 16:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (39.52%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        /*
+        - Brute
+          - convert n into binary string
+          - count # of ones
+        */
+
         int cnt = 0;
         while (n) {
             if (n & 1)
