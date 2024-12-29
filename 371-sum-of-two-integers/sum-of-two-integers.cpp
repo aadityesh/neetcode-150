ea7a7be48e072d8ce4e576bfebe19d3673822fe9
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-29 18:09:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (16.06%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -2,12 +2,26 @@ class Solution {
 public:
     int getSum(int a, int b) {
 
+/*
+
+        2 - 010        
+        1 - 001
+        _________
+        3 - 011
+
+        7 - 111
+        2 - 010
+        _______
+        9 - 1001
+          - 
+
+*/
         int carry = 0;
 
         while (b) {
-            int carry = (a & b) << 1;
-            a = a ^ b;
-            b = carry;
+            carry = (a & b) << 1;
+            a = a ^ b; // partial sum
+            b = carry; // carry
         }
         return a;
     }
