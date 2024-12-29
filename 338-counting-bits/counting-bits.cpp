c028aa6c09a5a1ae73e3544f5c6f078786928a93
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-29 13:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (41.57%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    // int numberOfOnes(int n) {
-    //     int cnt = 0;
-    //     while (n) {
-    //         if (n & 1)
-    //             cnt++;
-    //         n = n >> 1;
-    //     }
-    //     return cnt;
-    // }
     vector<int> countBits(int n) {
-        vector<int> res(n + 1, 0);
+
+        vector<int> result(n + 1, 0);
         for (int i = 0; i <= n; i++) {
-            res[i] = res[i / 2] + (i % 2);
+            result[i] = result[i / 2] + (i % 2);
         }
-        return res;
+
+        return result;
     }
 };
