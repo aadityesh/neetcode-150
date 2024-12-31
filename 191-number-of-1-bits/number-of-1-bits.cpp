2cb8edbf40c565ebab981b19941e35123ccaf890
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-31 18:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (7.38%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -7,12 +7,36 @@ public:
           - count # of ones
         */
 
-        int cnt = 0;
-        while (n) {
-            if (n & 1)
-                cnt++;
-            n = n >> 1;
+        /* int cnt = 0;
+         while (n) {
+             if (n & 1)
+                 cnt++;
+             n = n >> 1;
+         }
+         return cnt;
+         */
+
+        int table[256];
+        table[0] = 0;
+
+        for (int i = 0; i < 256; i++) {
+            table[i] = table[i / 2] + (i % 2);
         }
+
+        int cnt = 0;
+
+        cnt = table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
         return cnt;
     }
 };
