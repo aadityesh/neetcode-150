ef1ac7263de0d410ddefc1bd492b418185342790
Aadityesh Jain <aadityeshjain@yahoo.com>
2024-12-31 19:16:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (12.30%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-        
-        if (n == 1)
-            return true;
+        if(n <= 0) return false;
 
-        if (n & 1 || n == 0)
-            return false;
+        int table[256];
+        table[0] = 0;
 
-        while (n) {
-            n /= 2;
-            if (n & 1 && n != 1) return false;
+        for (int i = 0; i < 256; i++) {
+            table[i] = table[i / 2] + (i % 2);
         }
-        return true;
+
+        int cnt = 0;
+
+        cnt = table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
+        cnt += table[n & 0xff];
+        n = n >> 8;
+
+        return cnt == 1;
     }
 };
