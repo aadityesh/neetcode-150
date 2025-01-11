2ea3f09faf9679f2b8b2cb2671b80959472b5bae
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-01-11 08:01:54 +0530

    Time: 1 ms (45.75%) | Memory: 10.8 MB (5.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -7,28 +7,40 @@ public:
         int L = 0;
         int R = n - 1;
 
-        while (L < R) {
+        /* while (L < R) {
 
-            char left = toupper(s[L]);
-            char right = toupper(s[R]);
+             char left = toupper(s[L]);
+             char right = toupper(s[R]);
 
-            if (!isalnum(left)) {
-                L++;
-                continue;
-            }
+             if (!isalnum(left)) {
+                 L++;
+                 continue;
+             }
 
-            if (!isalnum(right)) {
-                R--;
-                continue;
-            }
+             if (!isalnum(right)) {
+                 R--;
+                 continue;
+             }
 
-            if (left != right)
-                return false;
+             if (left != right)
+                 return false;
 
+             L++;
+             R--;
+         }
+
+         return true;*/
+
+        string temp = "";
+        while (L <= R) {
+            if (isalnum(s[L]))
+                temp += tolower(s[L]);
             L++;
-            R--;
         }
+        cout << temp << endl;
+        string temp_cpy = temp;
+        reverse(temp.begin(), temp.end());
 
-        return true;
+        return temp_cpy == temp;
     }
 };
