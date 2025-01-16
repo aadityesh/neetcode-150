ef3ec1f33ce3c2e88ec5d7ac75a055abbedcaa91
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-01-16 12:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (26.76%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,76 +1,34 @@
 class Solution {
 public:
-    // pref, suff -> Brute (min(L, R) - height[i])
-    /*
-    vector<int> pref(vector<int> a) {
-
-        int n = a.size();
-        int maxi = 0;
-        int idx = 0;
-        vector<int> p(n, 0);
-
-        for (auto i : a) {
-            p[idx++] = maxi;
-            maxi = max(maxi, i);
-        }
-
-        return p;
-    }
-
-    vector<int> suff(vector<int> a) {
-
-        int n = a.size();
-        int maxi = 0;
-        int idx = n - 1;
-        vector<int> p(n, 0);
-
-        for (int i = n - 1; i >= 0; i--) {
-            p[idx--] = maxi;
-            maxi = max(maxi, a[i]);
-        }
-
-        return p;
-    }
-    */
-
     int trap(vector<int>& height) {
 
         int n = height.size();
-        /*
-        vector<int> prefix = pref(height);
-        vector<int> suffix = suff(height);
+        int L = 0;
+        int R = 0;
         int res = 0;
 
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+
+        int maxi = 0;
         for (int i = 0; i < n; i++) {
-            int curr = min(prefix[i], suffix[i]) - height[i];
-            if(curr >= 0) res += curr;
-
-            cout << curr << endl;
+            prefix[i] = maxi;
+            if (height[i] > maxi)
+                maxi = height[i];
+        }
+        maxi = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = maxi;
+            if (height[i] > maxi)
+                maxi = height[i];
         }
 
-        return res;
-        */
-
-        int res = 0;
-        int left = 0;
-        int right = n - 1;
-        int leftMax = height[0];
-        int rightMax = height[n - 1];
-
-        while (left < right) {
-
-            if(leftMax <= rightMax) {
-                left++;
-                leftMax = max(leftMax, height[left]);
-                res += (leftMax - height[left]);
-
-            } else {
-                right--;
-                rightMax = max(rightMax, height[right]);
-                res += (rightMax - height[right]);
+        for(int i = 0; i < n; i++) {
+            if(height[i] < min(prefix[i], suffix[i])) {
+                res += (min(prefix[i], suffix[i]) - height[i]);
             }
-
         }
+
         return res;
     }
 };
