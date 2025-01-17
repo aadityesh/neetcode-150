e66851f990333cf71c6b60cb36b88ee41e4fcee8
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-01-17 14:45:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (54.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (char& i : s) {
-            if (i == '(' || i == '{' || i == '[')
-                st.push(i);
-            else {
 
-                if (!st.empty()) {
+        stack<char> st;
+
+        for (char& i : s) {
+            if (i == '(' || i == '{' || i == '[') {
+                st.push(i);
+            } else {
+                
+                // here, we check whether for a closing brac, we have a open 
+                // brac or not
+                if (!st.empty()) { 
+
                     if (i == ')' && st.top() == '(')
                         st.pop();
                     else if (i == ']' && st.top() == '[')
@@ -21,8 +26,7 @@ public:
                     return false;
             }
         }
-        if (st.empty())
-            return true;
-        return false;
+
+        return st.empty(); // this handles odd lengths of inp
     }
 };
