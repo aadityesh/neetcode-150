8f104f79d8b37228dafbd2343ad6086b083f5583
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-01-17 16:27:47 +0530

    Time: 5 ms (31.43%) | Memory: 23.3 MB (47.59%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,32 +1,41 @@
 class MinStack {
-    stack<int> st1;
+
+    /*stack<int> st1;
     stack<int> st2;
+    */
+
+    vector<pair<int, int>> vec;
 
 public:
     MinStack() {}
 
     void push(int x) {
+
+        /*
         st1.push(x);
         if (st2.empty() || st2.top() >= x)
             st2.push(x);
+        */
+
+        if (vec.empty()) {
+            vec.push_back({x, x});
+            return;
+        };
+
+        int lastMin = vec.back().second;
+        lastMin = lastMin > x ? x : lastMin;
+        vec.push_back({x, lastMin});
     }
 
     void pop() {
-        if (st1.top() == st2.top())
-            st2.pop();
-        st1.pop();
+
+        // if (st1.top() == st2.top())
+        //     st2.pop();
+        // st1.pop();
+        return vec.pop_back();
     }
 
-    int top() { return st1.top(); }
+    int top() { return vec.back().first; }
 
-    int getMin() { return st2.top(); }
+    int getMin() { return vec.back().second; }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
