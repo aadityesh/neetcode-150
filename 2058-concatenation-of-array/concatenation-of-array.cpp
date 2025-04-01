0649c3f9ff758dd5bdf2476f072acfc370ec65ad
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-04-01 21:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (60.56%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -11,9 +11,13 @@ public:
         */
 
         int n = nums.size();
-        for (int i = 0; i < n; i++)
-            nums.push_back(nums[i]);
-        
-        return nums;
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
     }
 };
