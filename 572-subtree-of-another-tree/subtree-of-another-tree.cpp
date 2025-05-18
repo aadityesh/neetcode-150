ceea8f517f38693ecdb65d4c429e1a73ffcc3ad7
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-05-18 23:50:40 +0530

    Time: 1 ms (43.53%) | Memory: 30.1 MB (7.37%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -1,55 +1,55 @@
+
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& res) {
-        if (root == NULL)
-            return;
-        res.push_back(root->val);
-        preorder(root->left, res);
-        preorder(root->right, res);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
 
         if (p == NULL && q == NULL)
             return true;
-
-        if (p == NULL || q == NULL)
+        if (p == NULL && q != NULL)
             return false;
-
+        if (p != NULL && q == NULL)
+            return false;
         if (p->val != q->val)
             return false;
 
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 
+    vector<TreeNode*> BFS(TreeNode* root) {
+
+        if (root == NULL)
+            return {};
+
+        queue<TreeNode*> q;
+        vector<TreeNode*> vec;
+        q.push(root);
+
+        while (!q.empty()) {
+
+            int size = q.size();
+
+            while (size--) {
+
+                TreeNode* curr = q.front();
+                q.pop();
+                if (curr->left != NULL)
+                    q.push(curr->left);
+                if (curr->right != NULL)
+                    q.push(curr->right);
+
+                vec.push_back(curr);
+            }
+        }
+        return vec;
+    }
+
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
 
-        // vector<int> subtree;
-        // preorder(subRoot, subtree); // 4 1 2
-
-        // // TreeNode* curr = root;
-        // vector<int> tree;
-        // preorder(root, tree) // 3 4 1 2 5
-
-        // for(int i = 0; i < subtree.size(); i++) {
-
-        //     bool flag = true;
-        //     for(int j = 0; j < tree.size(); j++) {
-
-        //         if(subtree[i])
-
-        //     }
-        // }
-
-        // if(subRoot == NULL)
-        //     return true;
-        
-        if (root == NULL)
-            return false;
-
-        if (isSameTree(root, subRoot))
-            return true;
-
-        return (isSubtree(root->left, subRoot) ||
-                isSubtree(root->right, subRoot));
+        vector<TreeNode*> nodes = BFS(root);
+        for (auto& node : nodes) {
+            if (isSameTree(node, subRoot))
+                return true;
+        }
+        return false;
     }
 };
