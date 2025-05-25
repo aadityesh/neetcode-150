a4e487dabf8b854ede89cc048ebc4cf7f23e4c08
Aadityesh Jain <aadityeshjain@yahoo.com>
2025-05-25 23:05:32 +0530

    Time: 26 ms (27.63%) | Memory: 23.5 MB (33.38%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,7 +7,40 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 
+class Solution {
+public:
+//p < q;
+    TreeNode* lowest(TreeNode* root, TreeNode*p, TreeNode* q) {
+        if (p == root || q == root) return root;
+        if (root == nullptr) return nullptr;
+        if (p->val < root->val && q->val > root->val) {
+            return root;
+        }
+
+        if (p->val >= root->val && q->val >= root->val) {
+            return lowest(root->right, p, q);
+        }
+
+        if (p->val <= root->val && q->val <= root->val) {
+            return lowest(root->left, p, q);
+        }
+
+        return root;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val < q->val) {
+            return lowest(root, p, q);
+        }
+        else return lowest(root, q,p);
+    }
+
+    
+};
+
+auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+*/
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
